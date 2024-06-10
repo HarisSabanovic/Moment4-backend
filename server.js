51cbc3f3543139958dd18ff877ec81198c9f89ea
Haris Sabanovic <hasa2303@student.miun.se>
2024-06-10 16:18:11 +0200

    anslöt till min mongodb databas

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-require("dotenv").config();
+const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
 
 let port = process.env.PORT
+
+
+//connect to mongodb
+mongoose.connect("mongodb://127.0.0.1:27017/users_database").then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log(error + " Error connecting to database")
+})
 
 //ROutes
 app.use("/api", authRoutes);
