3e6593ec2db9fd9ac511169ff3c370683ee27b06
Haris Sabanovic <hasa2303@student.miun.se>
2024-08-22 11:04:49 +0200

    funktion som validerar token

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +17,25 @@ app.use("/api", authRoutes);
 app.get("/api/protected", authenticateToken, (req, res) => {
     res.json({ message: "Inloggad på skyddad server"})
 })
+
+//validate token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(token == null) {
+        res.status(401).json({ message: "not authorized"});
+    }
+
+    jwt.verify(token, process.env.JWT_KEY, (err, username) => {
+        if(err) {
+            return res.status(403).json({ message: "invalid jwt"});
+        }
+
+        req.username = username;
+        next();
+    });
+}
 
 // Starta applikation
 app.listen(port, () => {
